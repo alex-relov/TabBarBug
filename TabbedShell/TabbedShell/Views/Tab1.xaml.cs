67ccc6807eeaf67b3bf3e51080fb26191ac7bd85
alex <info@alex-relov.ru>
2020-07-24 11:03:43 +0500

    Add project files.

---
+++ TabbedShell/TabbedShell/Views/Tab1.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TabbedShell.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Tab1 : ContentPage
+    {
+        public Tab1()
+        {
+            InitializeComponent();
+        }
+
+        private async void Page1(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Page1());
+        }
+    }
+}
---
+++ TabbedShell/TabbedShell/Views/Tab2.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TabbedShell.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Tab2 : ContentPage
+    {
+        public Tab2()
+        {
+            InitializeComponent();
+        }
+    }
+}
